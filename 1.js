8557e7aecca6c6f36ed476168be3f835ea026fe1
Artem <aliev.artem5@gmail.com>
2019-10-16 18:31:59 +0300

    Save1

---
+++ 1.js
@@ -0,0 +1,13 @@
+var incr = 5,
+    decr = 5; 
+
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(123 === 123);
+
+let Dota5k = false, 
+dota3k = false;
+console.log(Dota5k || !dota3k);
+
